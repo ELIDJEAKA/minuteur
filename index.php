b501b156c7bc0d3d7c6dd5524cd92b51ab59f356
Kim-Taing SIMIN <kimtonlines@gmail.com>
2018-02-14 00:42:22 +0000

    Ajustement des bordures 6

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 <head>
     <meta http-equiv="refresh" content="30">
     <meta http-equiv="Content-Type" content="text/html"; charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="./bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="./style.css">
     <title>Chrono</title>
@@ -14,10 +15,10 @@
 <body>
 <div class="content col-lg-12 col-md-12 col-sm-12 col-xs-12" >
     <div class="content-top col-lg-12 col-md-12 col-sm-12 col-xs-12" > 
-        <span class="da" id="date_heure" style="font-size:25em;margin-top:50px;"></span>
+        <span class="da" id="date_heure" style="font-size:35em;margin-top:50px;"></span>
     </div>
     <div class="content-bottom col-lg-12 col-md-12 col-sm-12 col-xs-12">
-        <span id="minuteur" style="font-size:22em;margin-top:50px;"></span>
+        <span id="minuteur" style="font-size:25em;margin-top:50px;"></span>
     </div>
 </div>
 <script type="text/javascript">
